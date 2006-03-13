6ae6ddc29aa293fd3b4d98e8097ac73b29e71a05
Pistos <gitsomegrace.5.pistos@geoshell.com>
2006-03-13 04:29:15 +0000

    Spider actually works now...
    git-svn-id: http://localhost/svn/reby@794 3bcaa56b-69e1-0310-823e-bac524374bd6

--- spider-words.rb
+++ spider-words.rb
@@ -9,6 +9,8 @@ require 'word-ar-defs'
 
 
 class WordSpider
+    MIN_WORD_LENGTH = 4
+    HIGH_QUEUE_LEVEL = 20
     
     def initialize( seed_word )
         @seen_words = Array.new
@@ -27,48 +29,45 @@ class WordSpider
         while not @next_words.empty?
             @word = @next_words.shift
             getWord
-            if @next_words.size < 50
+            if @next_words.size < HIGH_QUEUE_LEVEL
                 gatherRelated
             end
+            $stderr.puts "words left: #{@next_words.size}"
         end
     end
     
     def gatherRelated
         other_words = Array.new
         open( "http://dictionary.reference.com/search?q=#{@word}" ) do |html|
-            $stderr.puts @word
-            
             soup = BeautifulSoup.new( html.read )
             
             soup.find_all( 'a', :attrs => { 'href' => /\/search\?q=[A-z]+/ } ).each do |a|
                 a[ 'href' ] =~ /([A-z]+)$/
                 other_word = $1
-                if other_word != nil and not other_word.empty?
+                if other_word != nil and other_word.length >= MIN_WORD_LENGTH
                     other_words << other_word
                 end
             end
         end
         
+        other_words.uniq!
         $stderr.puts other_words.join( '; ' )
-        retval = nil
         other_words.each do |word|
             if not @seen_words.include?( word ) and Word.find_by_word( word ).nil?
                 @next_words << word
-                break
             end
         end
     end
     
     def getWord
-        word = @word
-        @seen_words << word
+        @seen_words << @word
         catch( :problem ) do
-            $stderr.puts "** #{word}"
+            $stderr.puts "** #{@word}"
         
             definition = ''
             part_of_speech = ''
             
-            dict_ref = URI.parse( "http://dictionary.reference.com/search?q=#{word}" ).read
+            dict_ref = URI.parse( "http://dictionary.reference.com/search?q=#{@word}" ).read
             dict_lines = dict_ref.split( /\n/ )
             
             # Check for syllabification
@@ -77,7 +76,7 @@ class WordSpider
             dict_lines.each do |line2|
                 if line2 =~ /<b>([a-z]+(?:&#183;[a-z]+)*)<\/b>.* &nbsp;&nbsp;/
                     syllables = $1.split( /&#183;/ )
-                    if syllables.join( "" ) == word
+                    if syllables.join( "" ) == @word
                         syllabification = syllables
                         break
                     end
@@ -92,7 +91,7 @@ class WordSpider
             # Get Etymology
             
             etymology = ''
-            open( "http://dictionary.reference.com/search?q=#{word}" ) do |html2|
+            open( "http://dictionary.reference.com/search?q=#{@word}" ) do |html2|
                 soup2 = BeautifulSoup.new( html2.read )
                 
                 tag = soup2.find( 'hr' )
@@ -123,7 +122,7 @@ class WordSpider
                 # Two sources: wordsmyth.net and dictionary.com.
                 # Try one, then the other if first failed.
                 
-                open( "http://www.wordsmyth.net/live/home.php?script=search&matchent=#{word}&matchtype=exact" ) do |html3|
+                open( "http://www.wordsmyth.net/live/home.php?script=search&matchent=#{@word}&matchtype=exact" ) do |html3|
                     soup3 = BeautifulSoup.new( html3.read )
                     
                     span = soup3.find( 'span', :attrs => { 'class' => 'fieldValue', 'style' => 'font-weight: normal;' } )
@@ -188,7 +187,7 @@ class WordSpider
             end
             
             word_rec = Word.create( {
-                :word => word,
+                :word => @word,
                 :num_syllables => syllabification.length,
                 :pos => part_of_speech,
                 :etymology => etymology,
