adcabb1daf07c57551636ae7a52d03a52a9c9ee8
Pistos <gitsomegrace.5.pistos@geoshell.com>
2006-03-28 21:10:31 +0000

    Filled out title view.
    git-svn-id: http://localhost/svn/reby@877 3bcaa56b-69e1-0310-823e-bac524374bd6

--- railsapp/controllers/title_controller.rb
+++ railsapp/controllers/title_controller.rb
@@ -1,2 +1,20 @@
 class TitleController < ApplicationController
+    def index
+        redirect_to :action => 'list'
+    end
+    
+    def list
+        title_set_rows = TitleSet.find( :all, :order => :name )
+        @title_sets = Hash.new
+        title_set_rows.each do |row|
+            @title_sets[ row.name ] = Title.find(
+                :all,
+                :conditions => [
+                    "title_set_id = ?",
+                    row.id
+                ],
+                :order => :title_level_id
+            )
+        end
+    end
 end
