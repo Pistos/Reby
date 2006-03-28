db87c80d469ffaf6d36fea8b47bcd0524df515e2
Pistos <gitsomegrace.5.pistos@geoshell.com>
2006-03-28 20:29:00 +0000

    Added controllers.
    git-svn-id: http://localhost/svn/reby@873 3bcaa56b-69e1-0310-823e-bac524374bd6

---
+++ railsapp/controllers/player_controller.rb
@@ -0,0 +1,30 @@
+class PlayerController < ApplicationController
+    def index
+        redirect_to :action => 'list'
+    end
+    
+    def list
+        @players = Player.find( :all )
+        @players.delete_if { |p| p.games_played < 1 }
+        case params[ :sort ]
+            when 'rounds'
+                @players.sort! { |p1,p2| p2.games_played <=> p1.games_played }
+            when 'money'
+                @players.sort! { |p1,p2| p2.money <=> p1.money }
+            when 'nick'
+                @players.sort! { |p1,p2| p1.nick.downcase <=> p2.nick.downcase }
+            when 'rating'
+                @players.sort! { |p1,p2| p2.rating <=> p1.rating }
+            else
+                @players.sort! { |p1,p2| p2.rating <=> p1.rating }
+        end
+        
+        if params[ :reverse ]
+            @players.reverse!
+        end
+    end
+    
+    def view
+        @player = Player.find( params[ :id ].to_i )
+    end
+end
---
+++ railsapp/controllers/word_controller.rb
@@ -0,0 +1,2 @@
+class WordController < ApplicationController
+end
