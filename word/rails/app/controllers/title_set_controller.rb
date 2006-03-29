0ee0540d82eef1473f8d9d6f6586b7b1702d91e3
Pistos <gitsomegrace.5.pistos@geoshell.com>
2006-03-29 14:48:21 +0000

    Removing TitleSet stuff from rails.
    git-svn-id: http://localhost/svn/reby@893 3bcaa56b-69e1-0310-823e-bac524374bd6

--- word/rails/app/controllers/title_set_controller.rb
+++
@@ -1,9 +0,0 @@
-class TitleSetController < ApplicationController
-    def index
-        redirect_to :action => 'list'
-    end
-    
-    def list
-        @title_sets = TitleSet.find( :all )
-    end    
-end
--- word/rails/app/helpers/title_set_helper.rb
+++
@@ -1,2 +0,0 @@
-module TitleSetHelper
-end
