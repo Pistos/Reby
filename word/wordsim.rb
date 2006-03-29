b877b80941f0fb38a43568c60dd75084970d092e
Pistos <gitsomegrace.5.pistos@geoshell.com>
2006-03-29 22:19:02 +0000

    Made wordsim use RandomDotOrg.
    git-svn-id: http://localhost/svn/reby@899 3bcaa56b-69e1-0310-823e-bac524374bd6

--- word/wordsim.rb
+++ word/wordsim.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby
 
+require './random.rb'
+
+$generator = RandomDotOrg.new
+def random( ceiling = 0 )
+    $generator.generate( ceiling )[ 0 ]
+end
+
 class Player
     BASE_RATING = 2000
-    PLAYER_PROXIMITY = 0.05
+    # A player would be willing to play the nearest +/- 10% of players.
+    PLAYER_PROXIMITY = 0.1
     
     attr_reader :skill, :nick
     attr_accessor :rating
@@ -14,7 +22,7 @@ class Player
     end
     
     def agree_to_play?( opponent )
-        # A player would be willing to play the nearest +/- 5% of players.
+        # A player would be willing to play the nearest +/- PLAYER_PROXIMITY percent of players.
         return (
             @skill > opponent.skill - ( $simulator.skill_delta ) * ( $simulator.num_players * PLAYER_PROXIMITY )
         )
@@ -38,10 +46,10 @@ class Game
     end
     
     def run
-        outcome = rand * ( @p1.skill + @p2.skill )
+        outcome = random * ( @p1.skill + @p2.skill )
         initial_point_value = 100
         
-        point_value = ( initial_point_value * FRACTIONS[ rand( FRACTIONS.length ) ] ).to_i
+        point_value = ( initial_point_value * FRACTIONS[ random( FRACTIONS.length ) ] ).to_i
         
         if outcome < @p1.skill
             # Player 1 won
@@ -62,14 +70,17 @@ class Simulator
     
     def initialize
         @num_players = 100
-        @num_rounds = 100000
-        @skill_delta = 1.0 / @num_players 
+        @num_rounds = 10000
+        @top_skill = 9.0
+        @bottom_skill = 1.0
+        @skill_delta = ( @top_skill - @bottom_skill ) / @num_players 
+        
     end
     
     def run
         @players = []
         @num_players.times do |i|
-            @players << Player.new( "Player #{i}", @skill_delta * i )
+            @players << Player.new( "Player #{i}", @skill_delta * i + @bottom_skill )
         end
         
         @num_rounds.times do |round|
@@ -79,8 +90,8 @@ class Simulator
             p1 = nil
             p2 = nil
             loop do
-                p1 = @players[ rand( @players.size ) ]
-                p2 = @players[ rand( @players.size ) ]
+                p1 = @players[ random( @players.size ) ]
+                p2 = @players[ random( @players.size ) ]
                 if p1 != p2
                     if p1.agree_to_play?( p2 ) and p2.agree_to_play?( p1 )
                         # We found our opponents.
@@ -104,7 +115,7 @@ class Simulator
         
         puts
         players.each_with_index do |player,index|
-            puts "%-20s %d" % [ player.nick, player.rating ]
+            puts "%-20s %.3f\t%d" % [ player.nick, player.skill, player.rating ]
         end
     end
 end
