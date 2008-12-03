382656a4a21665f18cb86ed0d4b6ec59c51b1854
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-12-03 15:30:45 +0000

    Added polling of searched terms.
    git-svn-id: http://localhost/svn/reby@2654 3bcaa56b-69e1-0310-823e-bac524374bd6

--- reby-twitter.rb
+++ reby-twitter.rb
@@ -18,14 +18,31 @@ class RebyTwitter
     'manveru' => [ '#ramaze', '#mathetes', ],
     '_why' => [ '#ramaze', '#mathetes', ],
   }
+  SEARCHES = {
+    #'ramaze' => [ '#ramaze' ],
+    'ramaze' => [ '#mathetes' ],
+    'purepistos' => [ '#mathetes' ],
+    'm4dbi' => [ '#mathetes' ],
+    #'webbynode' => [ '#webbynode' ],
+  }
   
   def initialize
     config = YAML.load_file 'reby-twitter.yaml'
     @twitter = Twitter::Base.new( config[ 'username' ], config[ 'password' ] )
     @last_timestamp = Time.now
+    @last_search_id = Hash.new
+    
+    SEARCHES.each do |search_term,channels|
+      search = Twitter::Search.new( search_term )
+      fetched = search.fetch
+      @last_search_id[ search_term ] = fetched[ 'max_id' ]
+    end
+    
     @thread = Thread.new do
       loop do
-        poll_twitter
+        @echoed_ids = []
+        poll_timeline
+        poll_searches
         sleep 30
       end
     end
@@ -36,7 +53,7 @@ class RebyTwitter
     $reby.putserv "PRIVMSG #{destination} :#{message}"
   end
   
-  def poll_twitter
+  def poll_timeline
     tl = @twitter.timeline( :friends, :since => ( @last_timestamp + 1 ).to_s )
     if tl.any?
       tl.reverse!
@@ -45,16 +62,54 @@ class RebyTwitter
         say_tweet tweet
       end
     end
+  rescue Exception => e
+    $reby.log "RebyTwitter exception: #{e.message}"
+    #$reby.log e.backtrace.join( "\t\n" )
+  end
+  
+  def poll_searches
+    SEARCHES.each do |search_term,channels|
+      search = Twitter::Search.new( search_term )
+      last_id = @last_search_id[ search_term ] 
+      search.since( last_id )
+      fetched = search.fetch
+      if fetched[ 'max_id' ] > last_id
+        @last_search_id[ search_term ] = fetched[ 'max_id' ]
+        fetched[ 'results' ].each do |tweet|
+          say_search_tweet tweet, channels
+        end
+      end
+    end
+  rescue Exception => e
+    $reby.log "RebyTwitter exception: #{e.message}"
+    #$reby.log e.backtrace.join( "\t\n" )
   end
   
   def say_tweet tweet
+    if @echoed_ids.include? tweet.id
+      return
+    end
     src = tweet.user.screen_name
-    text = tweet.text.gsub( /[^a-zA-Z0-9,.;:!? _-]/, '' )
+    text = tweet.text.gsub( /[^a-zA-Z0-9,.;:'!?\/ _-]/, '' )
     alert = "[twitter] <#{src}> #{text}"
     channels = CHANNELS[ src ] || [ '#mathetes' ]
     channels.each do |channel|
       say alert, channel
     end
+    @echoed_ids << tweet.id
+  end
+  
+  def say_search_tweet( tweet, channels = [ '#mathetes' ] )
+    if @echoed_ids.include? tweet[ 'id' ]
+      return
+    end
+    src = tweet[ 'from_user' ]
+    text = tweet[ 'text' ].gsub( /[^a-zA-Z0-9,.;:'!?\/ _-]/, '' )
+    alert = "[twitter] <#{src}> #{text}"
+    channels.each do |channel|
+      say alert, channel
+    end
+    @echoed_ids << tweet[ 'id' ]
   end
 end
 
